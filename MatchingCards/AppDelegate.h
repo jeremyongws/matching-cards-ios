fd357242da1e5e587bc6470d18b97b4413598350
Jeremy Ong <jeremy.ongws@gmail.com>
2016-04-11 14:19:55 +0800

    Initial Commit

---
+++ MatchingCards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatchingCards
+//
+//  Created by Jeremy Ong on 11/04/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
